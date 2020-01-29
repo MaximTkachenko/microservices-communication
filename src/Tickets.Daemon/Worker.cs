c16ae196533e2af16696804e46c3e87ed0444d90
mtkachenko <oblomov86@gmail.com>
2020-01-29 20:21:06 +0100

    added samples to acquire access token using MSAL for client credential flow

--- src/Tickets.Daemon/Worker.cs
+++ src/Tickets.Daemon/Worker.cs
@@ -7,8 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using ClientCredential = Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential;
 using IHttpClientFactory = System.Net.Http.IHttpClientFactory;
+using TokenCache = Microsoft.IdentityModel.Clients.ActiveDirectory.TokenCache;
 
 namespace Tickets.Daemon
 {
@@ -20,6 +23,7 @@ namespace Tickets.Daemon
         private readonly ClientCredential _credential;
         private readonly string _usersApiBaseUrl;
         private readonly AzureAdOptions _azureAdConfig;
+        private IConfidentialClientApplication _clientApplication;
 
         public Worker(ILogger<Worker> logger,
             IHttpClientFactory http,
@@ -33,13 +37,24 @@ namespace Tickets.Daemon
             _authContext = new AuthenticationContext(_azureAdConfig.Authority, tokenCache);
             _credential = new ClientCredential(_azureAdConfig.ClientId, _azureAdConfig.ClientSecret);
             _usersApiBaseUrl = config.GetValue<string>("Services:UsersApiUrl");
+
+            var co = new ConfidentialClientApplicationOptions
+            {
+                Instance = "https://login.microsoftonline.com/",
+                TenantId = "6b9be1b6-4f80-4ce7-8479-16c4d7726470",
+                ClientId = _azureAdConfig.ClientId,
+                ClientSecret = _azureAdConfig.ClientSecret
+            };
+
+            _clientApplication = ConfidentialClientApplicationBuilder.CreateWithApplicationOptions(co)
+                .Build();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Worker running at: {time}", DateTime.Now);
 
                 var tokenResult = await _authContext.AcquireTokenAsync(_azureAdConfig.Resource, _credential);
 
@@ -53,6 +68,9 @@ namespace Tickets.Daemon
                 Console.WriteLine(claims);
 
                 await Task.Delay(5000, stoppingToken);
+
+                var result = await _clientApplication.AcquireTokenForClient(new []{"api://theapp.api/.default"})
+                    .ExecuteAsync(stoppingToken);
             }
         }
     }
